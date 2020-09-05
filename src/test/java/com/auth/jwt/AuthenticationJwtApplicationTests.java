83d44508114c417bd1c0b5c85a8ad088250d3da5
unknown <victor_marcantonio@hotmail.com>
2020-09-05 20:07:23 -0300

    subindo no github

---
+++ src/test/java/com/auth/jwt/AuthenticationJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
